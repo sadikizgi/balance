e66858a2ce556466298c9c0ea6c73fcb2bd03a1c
sadikizgi <sadik_izgi@hotmail.com>
2017-02-27 20:43:52 +0300

    sadikizgi

---
+++ NYP10022017/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYP10022017
+{
+    class Account
+    {
+      private decimal balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                if (value >= 0)
+                    balance = value;
+               
+            }
+        }
+        public void Credit(decimal amount)
+        {
+            balance = balance + amount;
+        }
+        public void WithDraw(decimal amount)
+        {
+            if (amount > balance)
+                Console.WriteLine("There is not enough money!");
+            else
+                balance = balance - amount;
+        }
+    }
+}
---
+++ NYP10022017/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYP10022017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account();
+            Account account2 = new Account();
+            account1.Balance = -5;
+            account2.Balance = 10.0M;
+            Console.WriteLine("Balance1: {0}",account1.Balance);
+            Console.WriteLine("Balance2: {0}", account2.Balance);
+            account1.Credit(15);
+            Console.WriteLine("Balance1: {0}", account1.Balance);
+            Console.WriteLine("Balance2: {0}", account2.Balance);
+            account1.WithDraw(5);
+            Console.WriteLine("Balance1: {0}", account1.Balance);
+            
+        }
+    }
+}
